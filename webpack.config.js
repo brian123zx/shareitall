491727e4d027cdf3457c5449ccb96afd70ccbee4
Brian Cooper <brianzx@gmail.com>
2017-02-06 20:23:43 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "cheap-eval-source-map"
+};
