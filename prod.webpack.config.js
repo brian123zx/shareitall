f1a04467152d0ff62f27425ff282bc02e28f40c8
Brian Cooper <brianzx@gmail.com>
2017-02-07 17:41:20 -0800

    add docs for prod

---
+++ prod.webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+module.exports = {
+  entry: {
+  	bundle: './js/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    // publicPath: 'docs/',
+    filename: '[name].js'
+  },
+  // devtool: "inline-source-map",
+  module: {
+	  rules: [{
+	    test: /\.js$/,
+	    exclude: /(node_modules|bower_components)/,
+	    use: {
+		    loader: 'babel-loader',
+		    options: {
+		      presets: ['es2015']
+			  }
+	    }
+	  }]
+  }
+};
